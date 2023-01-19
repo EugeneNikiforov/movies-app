9ab47fbf186b1d4f30d83eb9da63f7b75d3baef7
polrmn <polrmn@gmail.com>
2023-01-19 20:13:10 +0200

    add axios

--- src/js/fetchAPITrends.js
+++ src/js/fetchAPITrends.js
@@ -1,2 +1,14 @@
+import axios from 'axios';
+
+async function getMovieTrends() {
+
+    axios.defaults.baseURL = 'https://api.themoviedb.org/3/trending/all/week';
+    const result = axios.get('', {
+      params: {
+        api_key: '5e0ca358c6a85ef9a9e43b6452e61748',
+      },
+    });
+} 
+
 
 
